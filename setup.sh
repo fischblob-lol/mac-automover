603b7ffdf96c9ec5d3a3aaecf286f21d950e137d
fischblob-lol <235312011+fischblob-lol@users.noreply.github.com>
2026-02-27 19:22:51 +0100

    sorry bro

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 # beta, expect bugs dont rely on this 
 
-# make it fancy 
-fancy_hashtags = ###############
-fancy_hastags_single = #             #
 
 echo "this script is in beta, i wouldnt recommend relying on it for now"
 read -p "would you like to proceed? (Y/N) " answer
@@ -10,7 +7,7 @@ read -p "would you like to proceed? (Y/N) " answer
 case "$answer" in
     [Yy]* )
         echo "ok, ill clone the repository"
-        git clone https://fischblob-lol/mac-automover
+        git clone https://github.com/fischblob-lol/mac-automover
         echo "repository should be cloned now, i will execute the build script"
         echo "just kidding i dont have a build script"
         ;;
